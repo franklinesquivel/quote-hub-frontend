da1cd742ce989f2709280a8f9849419b4499fdbc
Franklin Esquivel <franklin.esquivel@outlook.com>
2023-05-29 23:23:46 -0600

    feat(store): Setup main app store.
    Registers `Quotes API`.

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
 import { ConfigProvider } from 'antd';
 import React              from 'react';
 import ReactDOM           from 'react-dom/client';
+import { Provider }       from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 import { router }         from '@/router';
+import { store }          from '@/store';
 import { mainTheme }      from '@/themes';
 import './main.css';
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <ConfigProvider theme={ mainTheme }>
-            <RouterProvider router={ router } />
-        </ConfigProvider>
+        <Provider store={ store }>
+            <ConfigProvider theme={ mainTheme }>
+                <RouterProvider router={ router } />
+            </ConfigProvider>
+        </Provider>
     </React.StrictMode>
 );
---
+++ src/store/index.ts
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { quotesApi }      from '@/api';
+
+
+export const store = configureStore({
+    reducer:    {
+        [quotesApi.reducerPath]: quotesApi.reducer
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(quotesApi.middleware)
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
