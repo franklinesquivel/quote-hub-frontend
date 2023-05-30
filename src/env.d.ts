9c9980e7064b30ee57cd6a67cf458023423e01ce
Franklin Esquivel <franklin.esquivel@outlook.com>
2023-05-29 23:16:33 -0600

    fix(types): Fixes Vite `env` typing.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,7 @@
-/// <reference types="vite/client" />
-
 interface ImportMeta {
-    readonly env: {
-        readonly VITE_API_NINJAS_API_KEY: string;
+    env: {
+        VITE_API_NINJAS_API_KEY: string;
     };
 }
+
+/// <reference types="vite/client" />
