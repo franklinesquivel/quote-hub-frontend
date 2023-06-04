e24c21d9b28639a28885d72315028a9023266237
guerretta16 <guerret98@gmail.com>
2023-06-04 10:46:07 -0600

    fix(constants): tags constants for api ninja were deleted by accident.
    - Tags were added and now it works.

---
+++ src/constants/api/tags.constants.ts
@@ -0,0 +1,7 @@
+export const LIST_ID_TAG = 'LIST';
+export const QUOTES_TAG = 'QUOTES';
+export const CATEGORIES_TAG = 'CATEGORIES';
+
+export const APININJAS_API_TAGS = [
+    LIST_ID_TAG, QUOTES_TAG
+];
