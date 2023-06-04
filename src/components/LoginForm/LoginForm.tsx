b232db030f6aa40c3155f0bb9e4d917632d1ef2d
guerretta16 <guerret98@gmail.com>
2023-06-04 10:34:22 -0600

    refactor(components): `LoginForm` was modified.
    - Form data type was changed to `LoginPayload` type.
    - Now just execute the function to get the access token.
    - Message component from ant was removed.

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,84 +1,68 @@
-import { Button, Form, Input, message }                                                      from 'antd';
-import { useState }                                                                          from 'react';
-import { useLazyGetAccessTokenQuery, useLazyGetAuthUserDataQuery, useLazyGetCsrfTokenQuery } from '@/api';
-import { setToken }                                                                          from '@/app';
-import { useAppDispatch }                                                                    from '@/hooks';
-import { UserLogin }                                                                         from '@/types';
+import { Button, Form, Input }        from 'antd';
+import { useState }                   from 'react';
+import { useLazyGetAccessTokenQuery } from '@/api';
+import { setToken }                   from '@/app';
+import { useAppDispatch }             from '@/hooks';
+import { LoginPayload }               from '@/types';
 import './LoginForm.css';
 
 
 export const LoginForm = () => {
 
-    const [getAccessToken, { isLoading: loadingAccessToken }] = useLazyGetAccessTokenQuery();
-    const [getCsrfToken, { isLoading: loadingCsrfToken }] = useLazyGetCsrfTokenQuery();
-    const [getAuthUserInfo] = useLazyGetAuthUserDataQuery();
+    const [getAccessToken, { isLoading }] = useLazyGetAccessTokenQuery();
     const [messageError, setMessageError] = useState('');
-    const [messageApi, contextHelper] = message.useMessage();
     const dispatch = useAppDispatch();
 
-    const onFinish = async (userData: UserLogin) => {
+    const onFinish = async (userData: LoginPayload) => {
         try {
-            await getCsrfToken(null).unwrap();
-            getAccessToken(userData)
-                .unwrap()
-                .then(token => {
-                    dispatch(setToken(token));
-                    getAuthUserInfo(token);
-                })
-                .catch(error => {
-                    setMessageError(error.data.message);
-                });
+            const token = await getAccessToken(userData).unwrap();
+            dispatch(setToken(token));
         } catch ( err ) {
-            messageApi.open({
-                type: 'error',
-                content: 'A server error occurred!!!'
-            });
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            setMessageError(err.data.message);
         }
     };
 
     return (
-        <>
-            { contextHelper }
-
-            <Form
-                autoComplete='off'
-                initialValues={ { remember: true } }
-                name='basic'
-                onFinish={ onFinish }
+        <Form
+            autoComplete='off'
+            initialValues={ { remember: true } }
+            name='basic'
+            onFinish={ onFinish }
+        >
+            <Form.Item
+                label='Username or email'
+                name='user_identifier'
+                rules={ [{ required: true, message: 'Please input your username or email!' }] }
             >
-                <Form.Item
-                    label='Username or email'
-                    name='user_identifier'
-                    rules={ [{ required: true, message: 'Please input your username or email!' }] }
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label='Password'
+                name='password'
+                rules={ [{ required: true, message: 'Please input your password!' }] }
+            >
+                <Input.Password />
+            </Form.Item>
+
+            <div className='login-error'>
+                {
+                    messageError && <span className='login-error-message'>{ messageError }</span>
+                }
+            </div>
+
+            <Form.Item wrapperCol={ { offset: 10 } }>
+                <Button
+                    htmlType='submit'
+                    loading={ isLoading }
+                    type='primary'
                 >
-                    <Input />
-                </Form.Item>
+                    Login
+                </Button>
 
-                <Form.Item
-                    label='Password'
-                    name='password'
-                    rules={ [{ required: true, message: 'Please input your password!' }] }
-                >
-                    <Input.Password />
-                </Form.Item>
-
-                <div className='login-error'>
-                    {
-                        messageError && <span className='login-error-message'>{ messageError }</span>
-                    }
-                </div>
-
-                <Form.Item wrapperCol={ { offset: 10 } }>
-                    <Button
-                        htmlType='submit'
-                        loading={ loadingAccessToken || loadingCsrfToken }
-                        type='primary'
-                    >
-                        Login
-                    </Button>
-
-                </Form.Item>
-            </Form>
-        </>
+            </Form.Item>
+        </Form>
     );
 };
